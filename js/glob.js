50035da4da2492e773e4036526f093efc058bf20
frsdgh <index2.mailbox@gmail.com>
2023-03-12 12:58:21 +0330

    Create

---
+++ js/glob.js
@@ -0,0 +1,7 @@
+/*   to disappear searchUser button in search while typing   */
+function disSearchUser() {
+    document.getElementById("userSearchBtn").style.display = "none";
+  }
+function shoSearchUser() {
+    document.getElementById("userSearchBtn").style.display = "flex";
+  }
