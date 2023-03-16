63e6ae94448ab151fd4f61306f971c61bf0c367b
frsdgh <index2.mailbox@gmail.com>
2023-03-16 12:36:01 +0330

    update style & add loading page

--- js/glob.js
+++ js/glob.js
@@ -1,7 +1,32 @@
 /*   to disappear searchUser button in search while typing   */
-function disSearchUser() {
-    document.getElementById("userSearchBtn").style.display = "none";
+// function disSearchUser() {
+//   document.getElementById("userSearchBtn").style.display = "none";
+// }
+
+// function shoSearchUser() {
+//   document.getElementById("userSearchBtn").style.display = "flex";
+// }
+
+function displaySearchUser() {
+  userSearchBtn = document.getElementById("userSearchBtn");
+  if (userSearchBtn.style.display === 'none') {
+    userSearchBtn.style.display = 'flex'
+  } else {
+    userSearchBtn.style.display = 'none'
   }
-function shoSearchUser() {
-    document.getElementById("userSearchBtn").style.display = "flex";
+}
+
+// function displaySearchUser() {
+//   document.getElementById("userSearchBtn").classList.toggle("disFlex");
+// }
+
+function appearLoading(value) {
+  if (value == 0) {
+    document.getElementById("appearLoading").style.display = "none";
+    document.getElementById("conInputs").style.borderRadius = "8px";
+  } else {
+    document.getElementById("appearLoading").style.display = "block";
+    document.getElementById("conInputs").style.borderRadius = "8px 8px 0 0";
+    document.getElementById("wrapLoader").classList.add("loader");
   }
+}
